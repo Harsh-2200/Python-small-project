1449842aacc9688036dd82f32cb941f5343c768d
Harsh-2200 <harshharodenice1@gmail.com>
2021-11-16 22:46:08 +0530

    initial

---
+++ GUI-Clock/main.py
@@ -0,0 +1,31 @@
+
+from tkinter import *
+from tkinter.ttk import *
+ 
+
+from time import strftime
+ 
+
+root = Tk()
+root.title('Clock')
+ 
+
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text = string)
+    label.after(1000, time)
+ 
+
+
+label = Label(root, font = ('calibri', 40, 'bold'),
+            background = 'light blue',
+            foreground = 'white')
+ 
+
+
+label.pack()
+time()
+ 
+mainloop()
+
